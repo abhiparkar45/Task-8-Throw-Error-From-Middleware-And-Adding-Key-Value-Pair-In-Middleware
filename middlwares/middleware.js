66c4bbec318c7fb9b566b6b28c5466aa4fd80307
abhiparkar45 <abhishekparkar48@gmail.com>
2022-11-15 10:14:40 +0530

    v2.0.0

--- middlwares/middleware.js
+++ middlwares/middleware.js
@@ -1,14 +1,13 @@
-const middleware = (req,res,next) =>{
-    if(req.params.action === "showerror"){
-        res.status(400).json({
-            success:false,
-            message:"throwing error from middleware"
-        })
-    }
-    else{
+const middleware = (req, res, next) => {
+  if (req.params.action === "showerror") {
+    return res.status(400).json({
+      success: false,
+      message: "throwing error from middleware",
+    });
+  } else {
     req.key = "value";
     next();
-    }
-}
+  }
+};
 
 module.exports = middleware;
